fb7f8d884a237b1543442db239a139cdfe0b1f54
Matheus Calixto <calixtinn@gmail.com>
2022-03-21 16:59:25 -0300

    Cconfiguração do VueRouter

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,14 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    // Caminho para acessar a rota
+    path: '/pedidos',
+    // View da rota
+    name: 'Pedidos',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "pedidos" */ '../views/Pedidos.vue')
   }
 ]
 
