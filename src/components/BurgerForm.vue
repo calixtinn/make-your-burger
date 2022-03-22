fd9ff2e6253a815906716c6284b2c43d9058fcad
Matheus Calixto <calixtinn@gmail.com>
2022-03-22 11:04:15 -0300

    Busca de itens do backend

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -11,29 +11,25 @@
               <label for="pao">Escolha o pão:</label>
               <select name="pao" id="pao" v-model="pao">
                   <option value="">Selecione o seu pão:</option>
-                  <option value="integral">Integral</option>
+                    <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">
+                      {{pao.tipo}}
+                    </option>
               </select>
           </div>
           <div class="input-container">
               <label for="carne">Selecione a carne:</label>
               <select name="carne" id="carne" v-model="carne">
                   <option value="">Selecione o tipo de carne</option>
-                  <option value="maminha">Maminha</option>
+                  <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">
+                      {{carne.tipo}}
+                    </option>
               </select>
           </div>
           <div id="opcionais-container" class="input-container">
               <label id="opcionais-title" for="carne">Selecione os opcionais:</label>
-              <div class="checkbox-container">
-                  <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
-                  <span>Salame</span>
-              </div>
-              <div class="checkbox-container">
-                  <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
-                  <span>Salame</span>
-              </div>
-              <div class="checkbox-container">
-                  <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
-                  <span>Salame</span>
+              <div v-for="opcional in opcionaisdata" :key="opcional.id" class="checkbox-container">
+                  <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
+                  <span>{{opcional.tipo}}</span>
               </div>
           </div>
           <div class="input-container">
@@ -49,8 +45,30 @@ export default {
     name: 'BurgerForm',
     data() {
         return {
-            nome: ''
+            nome: null,
+            paes: null,
+            carnes: null,
+            opcionaisdata: null,
+            pao: null,
+            carne: null,
+            opcionais: [],
+            status: "Solicitado",
+            msg: null
         }
+    },
+    methods: {
+        async getIngredientes() {
+            const req = await fetch('http://localhost:3000/ingredientes');
+            const data = await req.json();
+            console.log(data);
+
+            this.paes = data.paes;
+            this.carnes = data.carnes;
+            this.opcionaisdata = data.opcionais;
+        }
+    },
+    mounted() {
+        this.getIngredientes();
     }
 }
 </script>
