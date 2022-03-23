58b64953ff4522bedd6a2c39f68116786810202f
Matheus Calixto <calixtinn@gmail.com>
2022-03-23 15:49:59 -0300

    Substituindo tabela manual pelo datatable do primevue

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,162 +1,87 @@
 <template>
-  <div id="burger-table" v-if="burgers">
-    <div>
-      <div id="burger-table-heading">
-        <div class="order-id">#:</div>
-        <div>Cliente:</div>
-        <div>Pão:</div>
-        <div>Carne:</div>
-        <div>Opcionais:</div>
-        <div>Ações:</div>
-      </div>
-    </div>
-    <div id="burger-table-rows">
-      <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
-        <div class="order-number">{{ burger.id }}</div>
-        <div>{{ burger.nome }}</div>
-        <div>{{ burger.pao }}</div>
-        <div>{{ burger.carne }}</div>
-        <div>
-          <ul v-for="(opcional, index) in burger.opcionais" :key="index">
-            <li>{{ opcional }}</li>
-          </ul>
-        </div>
-        <div>
-          <select name="status" class="status" @change="updateBurger($event, burger.id)">
-            <option :value="s.tipo" v-for="s in status" :key="s.id" :selected="burger.status == s.tipo">
-              {{ s.tipo }}
-            </option>
-          </select>
-          <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
-        </div>
-      </div>
-    </div>
+  <div>
+    <Toast />
+    <DataTable :value="burgers" responsiveLayout="scroll" :stripedRows="true">
+        <Column field="id" header="#"></Column>
+        <Column field="nome" header="Nome"></Column>
+        <Column field="pao" header="Pão"></Column>
+        <Column field="carne" header="Carne"></Column>
+        <Column header="Opcionais">
+          <template #body="pedido">
+            <ul v-for="(opcional, index) in pedido.data.opcionais" :key="index">
+                <li>{{opcional}}</li>
+            </ul>
+          </template>
+        </Column>
+        <Column header="Status">
+          <template #body="pedido">
+            <Dropdown id="status" name="status" v-model="pedido.data.status" :options="status" optionLabel="tipo" optionValue="tipo" @change="updateBurger($event, pedido.data.id)"/>
+          </template>
+        </Column>
+        <Column :exportable="false" style="min-width:8rem">
+          <template #body="pedido">
+              <Button icon="pi pi-trash" class="p-button-rounded p-button-danger" @click="deleteBurger(pedido.data.id)" />
+          </template>
+        </Column>
+    </DataTable>
   </div>
-  <div v-else>
-    <h2>Não há pedidos no momento!</h2>
-  </div>
+
 </template>
 <script>
+  import DataTable from 'primevue/datatable';
+  import Column from 'primevue/column';
+  import Button from 'primevue/button';
+  import Dropdown from 'primevue/dropdown';
+  import api from '@/services/api'
+
+
   export default {
     name: "Dashboard",
+    components: {DataTable, Column, Button, Dropdown},
     data() {
       return {
         burgers: null,
         burger_id: null,
-        status: []
+        status: [],
       }
     },
     methods: {
       async getPedidos() {
-        const req = await fetch('http://localhost:3000/burgers')
-
-        const data = await req.json()
-
-        this.burgers = data
-
-        // Resgata os status de pedidos
-        this.getStatus()
-
+        await api.get('/burgers').then((response) => {
+          this.burgers = response.data;
+        });
       },
+
       async getStatus() {
-
-        const req = await fetch('http://localhost:3000/status')
-
-        const data = await req.json()
-
-        this.status = data
-
+        
+        await api.get('/status').then((response) => {
+          this.status = response.data;
+        });
       },
+
       async deleteBurger(id) {
 
-        const req = await fetch(`http://localhost:3000/burgers/${id}`, {
-          method: "DELETE"
+        await api.delete(`/burgers/${id}`).then((response) => {
+          this.$toast.add({severity: 'success', summary: 'Sucesso', detail:'Pedido deletado com sucesso', life:3000});
+          this.getPedidos();
         });
-
-        const res = await req.json()
-
-        this.getPedidos()
-
       },
+
       async updateBurger(event, id) {
 
-        const option = event.target.value;
-
-        const dataJson = JSON.stringify({status: option});
-
-        const req = await fetch(`http://localhost:3000/burgers/${id}`, {
-          method: "PATCH",
-          headers: { "Content-Type" : "application/json" },
-          body: dataJson
+        await api.patch(`/burgers/${id}`, {status: event.value}).then((response) => {
+          this.$toast.add({severity: 'success', summary: 'Sucesso', detail:'Status alterado com sucesso', life:3000});
+          this.getPedidos();
         });
-
-        const res = await req.json()
-
-        console.log(res)
-
       }
     },
     mounted () {
-    this.getPedidos()
+      this.getPedidos();
+      this.getStatus();
     }
   }
 </script>
 
 <style scoped>
-  #burger-table {
-    max-width: 1200px;
-    margin: 0 auto;
-  }
-
-  #burger-table-heading,
-  #burger-table-rows,
-  .burger-table-row {
-    display: flex;
-    flex-wrap: wrap;
-  }
-
-  #burger-table-heading {
-    font-weight: bold;
-    padding: 12px;
-    border-bottom: 3px solid #333;
-  }
-
-  .burger-table-row {
-    width: 100%;
-    padding: 12px;
-    border-bottom: 1px solid #CCC;
-  }
-
-  #burger-table-heading div,
-  .burger-table-row div {
-    width: 19%;
-  }
-
-  #burger-table-heading .order-id,
-  .burger-table-row .order-number {
-    width: 5%;
-  }
-
-  select {
-    padding: 12px 6px;
-    margin-right: 12px;
-  }
-
-  .delete-btn {
-    background-color: #222;
-    color:#fcba03;
-    font-weight: bold;
-    border: 2px solid #222;
-    padding: 10px;
-    font-size: 16px;
-    margin: 0 auto;
-    cursor: pointer;
-    transition: .5s;
-  }
-  
-  .delete-btn:hover {
-    background-color: transparent;
-    color: #222;
-  }
   
 </style>
